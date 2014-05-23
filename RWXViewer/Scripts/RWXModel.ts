4f72ec63b7861fa18034bb8c4910aaef0630ef2c
Mr. Byte <bytemr@gmail.com>
2014-05-22 20:41:23 -0600

    Added TypeScript support.

---
+++ RWXViewer/Scripts/RWXModel.ts
@@ -0,0 +1,9 @@
+﻿$(() => {
+    var canvas = <HTMLCanvasElement>document.getElementById("viewport");
+    var gl = canvas.getContext("webgl");
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+});
